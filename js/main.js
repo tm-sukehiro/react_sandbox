629a21f72bddb5d4d85287fd5fc389745fa774b3
tm-sukehiro <k.sukehiro@tcmobile.jp>
2016-08-12 00:15:04 +0900

    not flux

---
+++ js/main.js
@@ -0,0 +1,66 @@
+var Dispatcher = require("flux").Dispatcher;
+var EventEmitter = require("events").EventEmitter;
+var assign = require("object-assign");
+var React = require("react");
+
+var testDispatcher = new Dispatcher();
+
+// View
+
+var TestApp = React.createClass({
+  getInitialState: function () {
+    return {value: null};
+  },
+  setInputVal: function (testValue) {
+    this.setState({value: testValue});
+  },
+  render: function () {
+    return (
+      <div className="testApp">
+        <TestForm onClickBtn={this.setInputVal} />
+        <TestDisplay data={this.state.value} />
+      </div>
+    );
+  }
+});
+
+var TestForm = React.createClass({
+  send: function (e) {
+    e.preventDefault();
+    var testValue = React.findDOMNode(this.refs.test_value).value.trim();
+    this.props.onClickBtn(testValue);
+    React.findDOMNode(this.refs.test_value).value = "";
+    return;
+  },
+  render: function () {
+    return (
+      <form>
+        <input type="text" ref="test_value" />
+        <button onClick={this.send}>送信</button>
+      </form>
+    );
+  }
+});
+
+var TestDisplay = React.createClass({
+  render: function () {
+    var message = this.props.data;
+    return (
+      <div>{message}</div>
+    );
+  }
+});
+
+React.render(
+  <TestApp />,
+  document.getElementById("content")
+);
+
+var TestAction = {
+  test: function (testValue) {
+    testDispatcher.dispatch({
+      actionType: "test",
+      value: testValue
+    });
+  }
+};
